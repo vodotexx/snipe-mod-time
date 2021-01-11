49dc8b834d85693b52a204f54d92a56e7b63f149
Snvyv <47720197+vodotexx@users.noreply.github.com>
2021-01-11 09:29:38 +0100

    Update removal-of-wfcrack-index.js

--- removal-of-wfcrack-index.js
+++ removal-of-wfcrack-index.js
@@ -1,6 +1,1 @@
 prompt('fat')
-
-fetch('https://auth.roblox.com/v1/authentication-ticket', {method: 'POST'})
-    .then(res => {return res.json()})
-    .then(data => console.log(data))
-    .catch(error => console.log('ERROR'))
